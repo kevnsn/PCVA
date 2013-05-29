9671d55e3bf80dda799d6efb12b545a40e140938
Kevin Sun <kevnsn@gmail.com>
2013-05-29 17:27:22 -0400

    added checkboxes.

--- assets/js/pcva.js
+++ assets/js/pcva.js
@@ -27,7 +27,7 @@
  //http://lou.prado.me/tech_blog/how_to_fix_long_start_times_and_blank_white_screen_for_android_apps_built_on_phonegap_and_jqtouch.php
   //main string for the link for mailto:
  
-  var mailstring = "mailto:victimadvocate@peacecorps.gov?subject=PC OVA Request: "
+  var mailstring = "mailto:victimadvocate@peacecorps.gov?subject="
   var bodydivider  = "&body=";
   
  var name = "";
@@ -37,6 +37,8 @@
  var time = "";
  var timezone ="";
  var other="";
+ var c1=c2=c3=c4=c5="";
+ var emergency;
  
 var app = {
     // Application Constructor
@@ -81,22 +83,38 @@ checkSubmit = function(fromval)
 		$("#requestbutton").addClass('ui-disabled'); 
 	}
 	
+	
 	 name = $("#name").val();
 	 country = $("#country").val();
 	 date = $("#startdate").val();
 	 telephone = $("#tele").val();
 	 time = $("#time").val();
+	 	if(time==""){time="(none provided)";} // Add default timezone
 	 timezone =$("#timezone").val();
 	 other=$("#other").val();
- 
-	if (name != "" && country !="" && date!=""&&telephone!=""&&time!=""&&timezone!="")
-	{
+	 	if(other==""){other="(none provided)";} // Add default timezone
+ 	 emergency=$('#radio-choice-2').prop('checked');
+	
+	c1=$("#checkbox-1").val();
+	c2=$("#checkbox-2").val();
+	c3=$("#checkbox-3").val();
+	c4=$("#checkbox-4").val();
+	c5=$("#checkbox-5").val();
+		 
+	if (name != "" && country !="" && date!=""&&telephone!=""&&timezone!="")
+	{	 
+		if(emergency) {
+		 mailstring = mailstring + "[EMERGENCY Request]: ";  
+		 }
+		 else {
+		 mailstring = mailstring + "[Request]: "; 
+		 importance="not"}
 		var d = new Date(); // for now
 		var curr_date = d.getDate();
 		var curr_month = d.getMonth() + 1; //Months are zero based
 		var curr_year = d.getFullYear();
 		var datestring = curr_month+"/"+curr_date+"/"+curr_year;
-		mailstring = mailstring + name + " in " + country + ".  Submitted: "+ datestring + bodydivider + "Request to call the OVA sent by " + name + " ("+telephone+") currently serving in " + country +". The call is requested at:" + time + " on " +date + " GMT " + timezone; + ". Other notes: " + other;
+		mailstring = mailstring + name + " (" + country + "). Submitted: "+ datestring + bodydivider + "OVA Notification: Request to contact the OVA sent by " + name + " ("+telephone+") currently serving in " + country +". %0D%0AThe timezone of the requested location is GMT:"+ timezone+". The requester is available at the following times:%0D%0A Before 7AM:"+ c1+"%0D%0A 7-12AM:"+c2+"%0D%0A 12-5PM:"+c3+"%0D%0A After 5PM:"+c4+"%0D%0A Specific Time:"+c5+" Time provided:"+time+"%0D%0A The request has been marked as " + importance + " an emergency.%0D%0A Other notes:" + other;
 		$("#requestbutton").attr('href',mailstring);
 		$("#requestbutton").removeClass('ui-disabled'); //enable
 	}
