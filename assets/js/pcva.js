1b44346d8b89bc965f3b2ae448c499ec021309c6
Kevin Sun <kevnsn@gmail.com>
2013-05-29 17:32:14 -0400

    splash screen updates pt.2

--- assets/js/pcva.js
+++ assets/js/pcva.js
@@ -57,7 +57,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-		navigator.splashscreen.hide(); 
+		//navigator.splashscreen.hide(); 
         //app.change('requestbutton');
 		/*$("#requestbutton").addClass('ui-disabled');
 		$("#requestbutton").attr('href',mailstring); 
